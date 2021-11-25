e5e33ab583b98a01f3142f4bb25e21d5dc23586e
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-11-25 15:35:06 +0000

    Fixed the bugs

--- src/server.ts
+++ src/server.ts
@@ -6,95 +6,49 @@ import { Client } from "pg";
 //and default username and password,
 //we only need to specify the (non-default) database name.
 
-const client = new Client({ database: 'guestbook' });
-
-//TODO: this request for a connection will not necessarily complete before the first HTTP request is made!
-client.connect();
-
-
+const client = new Client({ database: "guestbook" });
 const app = express();
 
-/**
- * Simplest way to connect a front-end. Unimportant detail right now, although you can read more: https://flaviocopes.com/express-cors/
- */
-app.use(cors());
+//TODO: this request for a connection will not necessarily complete before the first HTTP request is made!
+client.connect().then(() => {
+  /**
+   * Simplest way to connect a front-end. Unimportant detail right now, although you can read more: https://flaviocopes.com/express-cors/
+   */
+  app.use(cors());
 
-/**
- * Middleware to parse a JSON body in requests
- */
-app.use(express.json());
+  /**
+   * Middleware to parse a JSON body in requests
+   */
+  app.use(express.json());
 
-//When this route is called, return the most recent 100 signatures in the db
-app.get("/signatures", async (req, res) => {
-  const signatures = null; //FIXME-TASK: get signatures from db!
-  res.status(200).json({
-    status: "success",
-    data: {
-      signatures
-    },
-  });
-});
-
-app.get("/signatures/:id", async (req, res) => {
-  // :id indicates a "route parameter", available as req.params.id
-  //  see documentation: https://expressjs.com/en/guide/routing.html
-  const id = parseInt(req.params.id); // params are always string type
-
-  const signature = null;   //FIXME-TASK get the signature row from the db (match on id)
-
-  if (signature) {
+  //When this route is called, return the most recent 100 signatures in the db
+  app.get("/signatures", async (req, res) => {
+    const signatures = await client.query(
+      "SELECT * FROM signatures LIMIT 100;"
+    ); //FIXME-TASK: get signatures from db!
     res.status(200).json({
       status: "success",
       data: {
-        signature,
+        signatures,
       },
     });
-  } else {
-    res.status(404).json({
-      status: "fail",
-      data: {
-        id: "Could not find a signature with that id identifier",
-      },
-    });
-  }
-});
+  });
 
-app.post("/signatures", async (req, res) => {
-  const { name, message } = req.body;
-  if (typeof name === "string") {
-    const createdSignature = null; //FIXME-TASK: insert the supplied signature object into the DB
+  app.get("/signatures/:id", async (req, res) => {
+    // :id indicates a "route parameter", available as req.params.id
+    //  see documentation: https://expressjs.com/en/guide/routing.html
+    const id = parseInt(req.params.id); // params are always string type
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        signature: createdSignature, //return the relevant data (including its db-generated id)
-      },
-    });
-  } else {
-    res.status(400).json({
-      status: "fail",
-      data: {
-        name: "A string value for name is required in your JSON body",
-      },
-    });
-  }
-});
+    const signature = await client.query(
+      "SELECT * FROM signatures WHERE id = $1",
+      [id]
+    ); //FIXME-TASK get the signature row from the db (match on id)
 
-//update a signature.
-app.put("/signatures/:id", async (req, res) => {
-  //  :id refers to a route parameter, which will be made available in req.params.id
-  const { name, message } = req.body;
-  const id = parseInt(req.params.id);
-  if (typeof name === "string") {
-
-    const result: any = null; //FIXME-TASK: update the signature with given id in the DB.
-
-    if (result.rowCount === 1) {
-      const updatedSignature = result.rows[0];
+    if (signature.rowCount !== 0) {
       res.status(200).json({
         status: "success",
         data: {
-          signature: updatedSignature,
+          signature,
         },
       });
     } else {
@@ -104,40 +58,96 @@ app.put("/signatures/:id", async (req, res) => {
           id: "Could not find a signature with that id identifier",
         },
       });
-
     }
-  } else {
-    res.status(400).json({
-      status: "fail",
-      data: {
-        name: "A string value for name is required in your JSON body",
-      },
-    });
-  }
-});
+  });
 
-app.delete("/signatures/:id", async (req, res) => {
-  const id = parseInt(req.params.id); // params are string type
+  app.post("/signatures", async (req, res) => {
+    const { name, message } = req.body;
+    if (typeof name === "string") {
+      const createdSignature = await client.query(
+        "INSERT INTO signatures (signature, message) VALUES ($1,$2) RETURNING *",
+        [name, message]
+      ); //FIXME-TASK: insert the supplied signature object into the DB
 
-  const queryResult: any = null; ////FIXME-TASK: delete the row with given id from the db  
-  const didRemove = queryResult.rowCount === 1;
+      res.status(201).json({
+        status: "success",
+        data: {
+          signature: createdSignature, //return the relevant data (including its db-generated id)
+        },
+      });
+    } else {
+      res.status(400).json({
+        status: "fail",
+        data: {
+          name: "A string value for name is required in your JSON body",
+        },
+      });
+    }
+  });
 
-  if (didRemove) {
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE#responses
-    // we've gone for '200 response with JSON body' to respond to a DELETE
-    //  but 204 with no response body is another alternative:
-    //  res.status(204).send() to send with status 204 and no JSON body
-    res.status(200).json({
-      status: "success",
-    });
-  } else {
-    res.status(404).json({
-      status: "fail",
-      data: {
-        id: "Could not find a signature with that id identifier",
-      },
-    });
-  }
+  //update a signature.
+  app.put("/signatures/:id", async (req, res) => {
+    //  :id refers to a route parameter, which will be made available in req.params.id
+    const { name, message } = req.body;
+    const id = parseInt(req.params.id);
+    if (typeof name === "string") {
+      const result: any = await client.query(
+        "UPDATE TABLE signatures SET name = $1, message = $2 WHERE id = $3",
+        [name, message, id]
+      ); //FIXME-TASK: update the signature with given id in the DB.
+
+      if (result.rowCount === 1) {
+        const updatedSignature = result.rows[0];
+        res.status(200).json({
+          status: "success",
+          data: {
+            signature: updatedSignature,
+          },
+        });
+      } else {
+        res.status(404).json({
+          status: "fail",
+          data: {
+            id: "Could not find a signature with that id identifier",
+          },
+        });
+      }
+    } else {
+      res.status(400).json({
+        status: "fail",
+        data: {
+          name: "A string value for name is required in your JSON body",
+        },
+      });
+    }
+  });
+
+  app.delete("/signatures/:id", async (req, res) => {
+    const id = parseInt(req.params.id); // params are string type
+
+    const queryResult: any = await client.query(
+      "DELETE FROM signatures WHERE id = $1",
+      [id]
+    ); ////FIXME-TASK: delete the row with given id from the db
+    const didRemove = queryResult.rowCount === 1;
+
+    if (didRemove) {
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE#responses
+      // we've gone for '200 response with JSON body' to respond to a DELETE
+      //  but 204 with no response body is another alternative:
+      //  res.status(204).send() to send with status 204 and no JSON body
+      res.status(200).json({
+        status: "success",
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+        data: {
+          id: "Could not find a signature with that id identifier",
+        },
+      });
+    }
+  });
 });
 
 export default app;
